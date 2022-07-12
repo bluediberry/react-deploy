8481e59b748d413818e247d372aa1787eba57117
Mariana Neto <32868723+bluediberry@users.noreply.github.com>
2022-07-12 23:29:06 +0100

    Updates

---
+++ precache-manifest.e514f0eac0da0ba9672de600e9ed8b4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2d3a1bec5ff27baa090",
+    "url": "/react-deploy/static/css/main.41471827.chunk.css"
+  },
+  {
+    "revision": "b2d3a1bec5ff27baa090",
+    "url": "/react-deploy/static/js/main.8091288a.chunk.js"
+  },
+  {
+    "revision": "cdad2344546541e355ba",
+    "url": "/react-deploy/static/js/runtime~main.a4152191.js"
+  },
+  {
+    "revision": "1a16e6e51f004993ec6b",
+    "url": "/react-deploy/static/js/2.fb6c9769.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/react-deploy/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-deploy/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "da36ae0f004ac11816cb6d530e725f56",
+    "url": "/react-deploy/index.html"
+  }
+];
